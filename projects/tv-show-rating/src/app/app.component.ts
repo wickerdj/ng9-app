603e1eb0049b6873407b908139fbd911758d56b4
Wick <wickerdj@gmail.com>
2020-07-19 16:30:28 -0500

    ExpressionChangedAfterItHasBeenCheckedError

--- projects/tv-show-rating/src/app/app.component.ts
+++ projects/tv-show-rating/src/app/app.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { TitleService } from './title.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  title$: Observable<string>;
+
   // title = 'tv-show-rating';
   title = null;
+
+  constructor(private titleSvc: TitleService) {}
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.title$ = this.titleSvc.title$;
+    });
+  }
 }
--- projects/tv-show-rating/src/app/tv-rating-form/tv-rating-form.component.ts
+++ projects/tv-show-rating/src/app/tv-rating-form/tv-rating-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { TitleService } from '../title.service';
 
 @Component({
   selector: 'app-tv-rating-form',
@@ -21,9 +22,11 @@ export class TvRatingFormComponent implements OnInit {
     rating: new FormControl('', Validators.required),
   });
 
-  constructor() {}
+  constructor(private titleSvc: TitleService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.titleSvc.update('new title!');
+  }
 
   submit() {
     alert(JSON.stringify(this.form.value));
